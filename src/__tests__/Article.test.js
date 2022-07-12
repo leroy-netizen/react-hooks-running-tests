11a9280c41e4ad3060fee17d8cc7650035efbef8
Leroy-netizen <leroythomas051@gmail.com>
2022-07-12 14:41:47 +0300

    Done.

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,17 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
